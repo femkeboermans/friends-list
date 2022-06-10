9845b5b0c2ffb57981ace56e1fcad65d5e37e324
Femke Boermans <femkeboermans@MacBook-Air-van-Femke.local>
2022-06-10 09:41:08 +0200

    initial commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,63 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li>phone: {{ phoneNumber }}</li>
+      <li>email: {{ emailAddress }}</li>
+    </ul>
+    <button @click="deleteFriend(id)">Delete Friend</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      validator: function (value) {
+        return value === false || value === true;
+      },
+    },
+  },
+  emits: ["toggle-favorite"],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavorite: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+    deleteFriend() {
+      this.$emit("delete-friend", this.id);
+    },
+  },
+};
+</script>
+
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label>E-mail</label>
+      <input type="email" v-model="EnteredEmail" />
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      enteredName: "",
+      enteredPhone: "",
+      EnteredEmail: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredPhone,
+        this.EnteredEmail
+      );
+    },
+  },
+};
+</script>
